345577c4bdc674f83cf4077e2ad6a5f740f599b6
cupkax <cupkax@protonmail.com>
2022-10-30 01:30:22 +1100

    Config Update and added new shader

--- shaders/SSimDownscaler.glsl
+++ shaders/SSimDownscaler.glsl
@@ -1,19 +1,18 @@
-// Source: https://gist.github.com/igv/36508af3ffc84410fe39761d6969be10
-
-// Requires linear-downscaling=no. Can be used with sharp scalers now (finally able to suppress ringing artifacts).
-
+// Revised 05/15/22
+// https://gist.github.com/igv/36508af3ffc84410fe39761d6969be10
+//
 // SSimDownscaler by Shiandow
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
 // version 3.0 of the License, or (at your option) any later version.
-//
+// 
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library.
 
