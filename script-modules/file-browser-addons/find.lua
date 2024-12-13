899313f64017f8f87fd9c3303a2312f7672271f3
cupkax <cupkax@protonmail.com>
2024-12-13 11:55:36 +1100

    update

--- script-modules/file-browser-addons/find.lua
+++ script-modules/file-browser-addons/find.lua
@@ -12,13 +12,11 @@
 
 local msg = require "mp.msg"
 local fb = require "file-browser"
-local success, input = pcall(require, 'mp.input')
-if not success then
-    user_input_loaded, input = pcall(require, "user-input-module")
-end
+local input_loaded, input = pcall(require, "mp.input")
+local user_input_loaded, user_input = pcall(require, "user-input-module")
 
 local find = {
-    version = "1.4.0"
+    version = "1.3.0"
 }
 local latest_coroutine = nil
 local global_fb_state = getmetatable(fb.get_state()).__original
@@ -38,26 +36,19 @@ local function main(key, state, co)
     if key.name == "find/find" then text = "Find: enter search string"
     else text = "Find: enter advanced search string" end
 
-    local query, error = nil, nil
-    if user_input_loaded then
-        query, error = coroutine.yield(
-            input.get_user_input( fb.coroutine.callback(), { text = text, id = "find", replace = true } )
-        )
-    elseif input then
-        query, error = coroutine.yield(
-            input.get({
-                prompt = text .. "\n>",
-                id = "find",
-                submit = fb.coroutine.callback(),
-            })
-        )
+    if input_loaded then
+        input.get({
+            prompt = text .. "\n>",
+            id = "file-browser/find",
+            submit = fb.coroutine.callback(),
+        })
+    elseif user_input_loaded then
+        user_input.get_user_input( fb.coroutine.callback(), { text = text, id = "find", replace = true } )
     end
 
-    if not query then
-        return msg.debug(error)
-    elseif query and not user_input_loaded then
-        input.terminate()
-    end
+    local query, error = coroutine.yield()
+    if input_loaded then input.terminate() end
+    if not query then return msg.debug(error) end
 
     -- allow the directory to be changed before this point
     local list = fb.get_list()
